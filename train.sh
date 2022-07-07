2aa17d36d0433a7fae75f8bab28a238359d3db41
Chuang Liu <33613547+Niculuse@users.noreply.github.com>
2022-07-08 00:51:04 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0,1 torchrun --master_port 29504 --nproc_per_node 2 train.py --gpu_id 2,3 \
 --dataset prw --batch_size 8 --logs_dir 'logs' --lr 0.009 --backbone resnet50_ibn_a
-#
+
 CUDA_VISIBLE_DEVICES=0,1 torchrun --master_port 29504 --nproc_per_node 2 train.py --gpu_id 2,3 \
 --dataset sysu --batch_size 8 --logs_dir 'logs' --lr 0.009 --backbone resnet50_ibn_a
 
